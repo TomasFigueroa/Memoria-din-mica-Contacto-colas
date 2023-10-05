30e1b9ee818f9d5e0d73cc000f907ae7bc54ce96
dstomasfigueroa@hotmail.com <dstomasfigueroa@hotmail.com>
2023-10-04 22:35:13 -0300

    Agregar archivos de proyecto.

---
+++ #73/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+class Programan
+{
+    public static void Main()
+{
+
+    Queue<string> cola = new Queue<string>();
+
+    while (true)
+    {
+        Console.WriteLine("Escriba las cadenas de texto que desee almacenar en la queue");
+        Console.WriteLine("o introduzca una cadena vacia para recibir lo almacenado en la queue");
+        Console.WriteLine("");
+
+        string datoEnQueue = Console.ReadLine();
+
+        Console.WriteLine("");
+
+        if (datoEnQueue == "")
+        {
+            MostrarQueue(cola);
+            break;
+        }
+        else
+        {
+            cola.Enqueue(datoEnQueue);
+        }
+    }
+
+    static void MostrarQueue(Queue<string> cola)
+    {
+        while (cola.Count > 0)
+        {
+            string dato = (string)cola.Dequeue();
+
+            Console.WriteLine(dato);
+        }
+    }
+}
+
+
+}
+
